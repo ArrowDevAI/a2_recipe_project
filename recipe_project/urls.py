6b6a0f4dc1a6641a51b3ae4203af946d9a4a57f7
Trevor Higgins <higginst.th@gmail.com>
2025-01-21 09:28:20 -0500

    removed recipes/ and corrected to / in urls.py

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -23,7 +23,7 @@ from .views import login_view, logout_view, logout_success_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('logout_success/', logout_success_view, name='logout_success'),
